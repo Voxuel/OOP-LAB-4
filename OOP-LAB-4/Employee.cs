4c60c5ac1a5e7dda8ab9e2667edad02b078736ec
Voxuel <Leo.Fridh@hotmail.com>
2022-10-30 12:27:24 +0100

    Added Employee class and created objects in main

---
+++ OOP-LAB-4/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_LAB_4
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public decimal Salery { get; set; }
+
+        public Employee(int _id, string _name, string _gender, decimal _salery)
+        {
+            ID = _id;
+            Name = _name;
+            Gender = _gender;
+            Salery = _salery;
+        }
+    }
+}
--- OOP-LAB-4/Program.cs
+++ OOP-LAB-4/Program.cs
@@ -6,6 +6,12 @@ namespace OOP_LAB_4
     {
         static void Main(string[] args)
         {
+            var john = new Employee(1000, "John", "Male", 23500);
+            var joe = new Employee(1001, "Joe", "Male", 45330);
+            var karen = new Employee(1002, "Karen", "Female", 23000);
+            var linda = new Employee(1003, "Linda", "Female", 40000);
+            var jack = new Employee(1004, "Jack", "Male", 15034);
+
 
         }
     }
